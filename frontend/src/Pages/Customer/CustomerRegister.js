2c14b5de5144049bcef035bb25a3670f1520de5f
Ronny <rkesh2003@gmail.com>
2023-07-08 11:55:25 +0530

    basic styling for customer registration and some global css

--- frontend/src/Pages/Customer/CustomerRegister.js
+++ frontend/src/Pages/Customer/CustomerRegister.js
@@ -1,8 +1,65 @@
+import { Button, TextField } from "@mui/material";
+
 function CustomerRegister() {
   return (
-    <div className="flex-center">
+    <div className="flex-center-full-hw">
       <form>
-        <p>Customer Register</p>
+        <h1>Customer Register</h1>
+        <div className="field-container">
+          <TextField
+            type="text"
+            id="first-name"
+            label="First Name"
+            required
+            variant="filled"
+            fullWidth
+          />
+        </div>
+        <div className="field-container">
+          <TextField
+            type="text"
+            id="last-name"
+            label="Last Name"
+            required
+            variant="filled"
+            fullWidth
+          />
+        </div>
+        <div className="field-container">
+          <TextField
+            type="email"
+            id="email"
+            label="Email"
+            required
+            variant="filled"
+            fullWidth
+          />
+        </div>
+        <div className="field-container">
+          <TextField
+            type="password"
+            id="password"
+            label="Password"
+            required
+            variant="filled"
+            fullWidth
+          />
+        </div>
+        <div className="field-container">
+          <TextField
+            type="password"
+            id="confirm-password"
+            label="Confirm password"
+            required
+            variant="filled"
+            fullWidth
+          />
+        </div>
+        <div className="field-container">
+          <Button variant="contained" type="submit" fullWidth>
+            Register
+          </Button>
+        </div>
       </form>
     </div>
   );
