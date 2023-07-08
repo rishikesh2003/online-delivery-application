86ae9ac46e9e153c1119ef6d72e2ceac001ef09a
yuvan060 <yuvansankar060@gmail.com>
2023-07-08 12:22:11 +0530

    added customer login page and setup functionalities

--- frontend/src/Pages/Customer/CustomerLogin.js
+++ frontend/src/Pages/Customer/CustomerLogin.js
@@ -1,5 +1,58 @@
+import { TextField, Button } from "@mui/material";
+import { useState } from "react";
 function CustomerLogin() {
-  return <h1>CustomerLogin</h1>;
+  const [customerLogin, setCustomerLogin] = useState({
+    email: "",
+    password: "",
+  });
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(customerLogin);
+  }
+  return (
+    <div className="flex-center-full-hw">
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <h1>Customer Login</h1>
+        <div className="field-container">
+          <TextField
+            required
+            value={customerLogin.email}
+            onChange={(e) => {
+              setCustomerLogin({ ...customerLogin, email: e.target.value });
+            }}
+            type="text"
+            id="email"
+            label="Email"
+            variant="filled"
+            fullWidth
+          />
+        </div>
+        <div className="field-container">
+          <TextField
+            required
+            value={customerLogin.password}
+            onChange={(e) => {
+              setCustomerLogin({ ...customerLogin, password: e.target.value });
+            }}
+            type="text"
+            id="password"
+            label="Password"
+            variant="filled"
+            fullWidth
+          />
+        </div>
+        <div className="field-container">
+          <Button type="submit" variant="contained" fullWidth>
+            Login
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CustomerLogin;
