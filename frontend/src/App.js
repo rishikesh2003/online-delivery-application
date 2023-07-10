cef10c4b0bb8ea8e4753a7885e899857eb21498b
Rishikesh S <rkesh2003@gmail.com>
2023-07-10 09:50:31 +0530

    added fallback component and setup home and conditional routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,32 @@ import DeliveryPartnerLogin from "./Pages/DeliveryPartner/DeliveryPartnerLogin";
 import DeliveryPartnerRegister from "./Pages/DeliveryPartner/DeliveryPartnerRegister";
 import ShopLogin from "./Pages/Shop/ShopLogin";
 import ShopRegister from "./Pages/Shop/ShopRegister";
+import Home from "./Pages/Home/Home";
+import Error from "./Pages/Error/Error";
 
 function App() {
+  const isLoggedIn = false;
   return (
     <Router>
-      <Routes>
-        <Route path="/customer-login" element={<CustomerLogin />} />
-        <Route path="/customer-register" element={<CustomerRegister />} />
-        <Route path="/del-partner-login" element={<DeliveryPartnerLogin />} />
-        <Route
-          path="/del-partner-register"
-          element={<DeliveryPartnerRegister />}
-        />
-        <Route path="/shop-login" element={<ShopLogin />} />
-        <Route path="/shop-register" element={<ShopRegister />} />
-      </Routes>
+      {isLoggedIn ? (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<CustomerLogin />} />
+          <Route path="/customer-register" element={<CustomerRegister />} />
+          <Route path="/del-partner-login" element={<DeliveryPartnerLogin />} />
+          <Route
+            path="/del-partner-register"
+            element={<DeliveryPartnerRegister />}
+          />
+          <Route path="/shop-login" element={<ShopLogin />} />
+          <Route path="/shop-register" element={<ShopRegister />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      )}
     </Router>
   );
 }
