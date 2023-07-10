e3d336c0467d9c820dfc9b9855b87b8edb25154d
Rishikesh S <rkesh2003@gmail.com>
2023-07-10 10:05:13 +0530

    combined login and register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import CustomerLogin from "./Pages/Customer/CustomerLogin";
-import CustomerRegister from "./Pages/Customer/CustomerRegister";
 import DeliveryPartnerLogin from "./Pages/DeliveryPartner/DeliveryPartnerLogin";
-import DeliveryPartnerRegister from "./Pages/DeliveryPartner/DeliveryPartnerRegister";
 import ShopLogin from "./Pages/Shop/ShopLogin";
-import ShopRegister from "./Pages/Shop/ShopRegister";
 import Home from "./Pages/Home/Home";
 import Error from "./Pages/Error/Error";
+import CustomerAuth from "./Pages/Customer/CustomerAuth";
 
 function App() {
   const isLoggedIn = false;
@@ -19,16 +16,9 @@ function App() {
         </Routes>
       ) : (
         <Routes>
-          <Route path="/" element={<CustomerLogin />} />
-          <Route path="/customer-register" element={<CustomerRegister />} />
-          <Route path="/del-partner-login" element={<DeliveryPartnerLogin />} />
-          <Route
-            path="/del-partner-register"
-            element={<DeliveryPartnerRegister />}
-          />
-          <Route path="/shop-login" element={<ShopLogin />} />
-          <Route path="/shop-register" element={<ShopRegister />} />
-          <Route path="*" element={<Error />} />
+          <Route path="/" element={<CustomerAuth />} />
+          <Route path="/del-partner-auth" element={<DeliveryPartnerLogin />} />
+          <Route path="/shop-auth" element={<ShopLogin />} />
         </Routes>
       )}
     </Router>
---
+++ frontend/src/Pages/Customer/CustomerAuth.js
@@ -0,0 +1,187 @@
+import { TextField, Button } from "@mui/material";
+import { useState } from "react";
+function CustomerAuth() {
+  const [customerLogin, setCustomerLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [customer, setCustomer] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [state, setState] = useState("login");
+
+  function handleLogin(e) {
+    e.preventDefault();
+    console.log(customerLogin);
+  }
+
+  function handleRegister(e) {
+    e.preventDefault();
+    if (customer.password !== customer.confirmPassword) {
+      alert("Passwords won't match");
+    } else {
+      console.log(customer);
+    }
+  }
+
+  return (
+    <div className="flex-center-full-hw">
+      <form
+        onSubmit={(e) => {
+          if (state === "login") {
+            handleLogin(e);
+          } else {
+            handleRegister(e);
+          }
+        }}
+      >
+        <div className="button-header">
+          <Button
+            onClick={() => {
+              setState("login");
+            }}
+          >
+            Login
+          </Button>
+          |
+          <Button
+            onClick={() => {
+              setState("register");
+            }}
+          >
+            Register
+          </Button>
+        </div>
+
+        <h1>Customer</h1>
+        {state === "login" ? (
+          <>
+            {" "}
+            <div className="field-container">
+              <TextField
+                required
+                value={customerLogin.email}
+                onChange={(e) => {
+                  setCustomerLogin({ ...customerLogin, email: e.target.value });
+                }}
+                type="email"
+                id="email"
+                label="Email"
+                variant="outlined"
+                fullWidth
+              />
+            </div>
+            <div className="field-container">
+              <TextField
+                required
+                value={customerLogin.password}
+                onChange={(e) => {
+                  setCustomerLogin({
+                    ...customerLogin,
+                    password: e.target.value,
+                  });
+                }}
+                type="password"
+                id="password"
+                label="Password"
+                variant="outlined"
+                fullWidth
+              />
+            </div>
+            <div className="field-container">
+              <Button type="submit" variant="contained" fullWidth>
+                Login
+              </Button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="field-container">
+              <TextField
+                value={customer.firstName}
+                onChange={(e) => {
+                  setCustomer({ ...customer, firstName: e.target.value });
+                }}
+                type="text"
+                id="first-name"
+                label="First Name"
+                required
+                variant="outlined"
+                fullWidth
+              />
+            </div>
+            <div className="field-container">
+              <TextField
+                value={customer.lastName}
+                onChange={(e) => {
+                  setCustomer({ ...customer, lastName: e.target.value });
+                }}
+                type="text"
+                id="last-name"
+                label="Last Name"
+                required
+                variant="outlined"
+                fullWidth
+              />
+            </div>
+            <div className="field-container">
+              <TextField
+                value={customer.email}
+                onChange={(e) => {
+                  setCustomer({ ...customer, email: e.target.value });
+                }}
+                type="email"
+                id="email"
+                label="Email"
+                required
+                variant="outlined"
+                fullWidth
+              />
+            </div>
+            <div className="field-container">
+              <TextField
+                value={customer.password}
+                onChange={(e) => {
+                  setCustomer({ ...customer, password: e.target.value });
+                }}
+                type="password"
+                id="password"
+                label="Password"
+                required
+                variant="outlined"
+                fullWidth
+              />
+            </div>
+            <div className="field-container">
+              <TextField
+                value={customer.confirmPassword}
+                onChange={(e) => {
+                  setCustomer({ ...customer, confirmPassword: e.target.value });
+                }}
+                type="password"
+                id="confirm-password"
+                label="Confirm password"
+                required
+                variant="outlined"
+                fullWidth
+              />
+            </div>
+            <div className="field-container">
+              <Button variant="contained" type="submit" fullWidth>
+                Register
+              </Button>
+            </div>
+          </>
+        )}
+      </form>
+    </div>
+  );
+}
+
+export default CustomerAuth;
